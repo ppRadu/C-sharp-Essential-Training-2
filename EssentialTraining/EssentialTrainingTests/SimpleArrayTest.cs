0345f57ce74d385870214d45af323a8824a07697
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-03-25 01:34:49 +0200

    Arrays and Unit Testing

---
+++ EssentialTraining/EssentialTrainingTests/SimpleArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SimpleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new SimpleArray();
+            Assert.AreEqual(4, testInstance.GroceryList.Length);
+            Assert.AreEqual("Milk", testInstance.GroceryList[1]);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new SimpleArray();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+        }
+    }
+}
