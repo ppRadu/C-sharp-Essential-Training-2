b96b76aa5135b7c355dfccda5a33a7ea7610a4ef
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-04-01 02:56:05 +0300

    Lists

---
+++ EssentialTraining/EssentialTrainingTests/AwesomeSauceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tabasco");
+            testInstance.Sauces.Add("Cholula");
+            testInstance.Sauces.Add("Ranch");
+
+            // expect pass
+            Assert.IsTrue(testInstance.IsSauceAwesome("Ranch"));
+
+            // expect false
+            Assert.IsFalse(testInstance.IsSauceAwesome("Ketchup"));
+        }
+    }
+}
