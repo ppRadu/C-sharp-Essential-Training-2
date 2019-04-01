7b30e2de59f715214b6ca9d0ac7ec57e154ee3e3
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-04-02 01:55:20 +0300

    OR AND NOT

--- EssentialTraining/EssentialTraining/FlowControl.cs
+++ EssentialTraining/EssentialTraining/FlowControl.cs
@@ -74,5 +74,47 @@ namespace EssentialTraining
             }
             return result;
         }
+
+        public string PrimaryOrSecondaryCompound(string color)
+        {
+            if (color.ToLower() == "red" || color.ToLower() == "green" || color.ToLower() == "blue")
+            {
+                return "Primary";
+            }
+            else
+            {
+                return "Secondary";
+            }
+        }
+        public string GradeLetter(int score)
+        {
+            if(score > 90)
+            {
+                return "A";
+            }
+            else if (score > 79 && score < 90)
+            {
+                return "B";
+            }
+            else if (score > 70 && score <= 80)
+            {
+                return "C";
+            }
+            else
+            {
+                return "F";
+            }
+        }
+        public string IsYourNameNotBruce(string yourName)
+        {
+            if(yourName.ToLower() != "bruce")
+            {
+                return "He's not Bruce...";
+            }
+            else
+            {
+                return "Howdy, Bruce!";
+            }
+        }
     }
 }
