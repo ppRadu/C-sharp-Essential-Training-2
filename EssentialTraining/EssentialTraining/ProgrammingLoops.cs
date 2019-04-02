394d6f4590a79f457b16904d099055cc754a7b45
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-04-02 20:53:51 +0300

    For and Foreach Loops

---
+++ EssentialTraining/EssentialTraining/ProgrammingLoops.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class ProgrammingLoops
+    {
+        public int ForLoop()
+        {
+            var sum = 0;
+
+            for(var i = 0; i < 100; i++)
+            {
+                sum += i;   
+            }
+
+            return sum;
+        }
+
+        public int ForEachLoop()
+        {
+            var numbers = new List<int> { 1, 3, 5, 7, 9 };
+            var sum = 0;
+
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
